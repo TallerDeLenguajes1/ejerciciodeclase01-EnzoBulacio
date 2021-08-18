e3edc6f3ad8963c769cba138fed51abaeea3dc8c
Enzobulacio18@outlook.com <Enzobulacio18@outlook.com>
2021-08-18 17:57:45 -0300

    falta do

---
+++ Azar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Azar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int cons = rand.Next(0, 101);
+            Console.WriteLine("Ingrese un numero: ");
+            int valor = Convert.ToInt32(Console.ReadLine());
+            if (cons > valor)
+            {
+                Console.WriteLine("Es mayor");
+            }
+            if(valor > cons){
+                Console.WriteLine("Es menor");
+            }
+            if(valor==cons)
+            {
+                Console.WriteLine("FELICITACIONES");
+            }
+        }
+    }
+}
